8fab315d3ce0770bbed11d6419f891359094d816
Julien Vey <julien.vey@zenika.com>
2011-03-07 23:21:52 +0100

    first commit

---
+++ images/src/main/java/com/julienvey/blog/gwtstuff/images/client/Resources.java
@@ -0,0 +1,32 @@
+package com.julienvey.blog.gwtstuff.images.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+
+public interface Resources extends ClientBundle {
+
+	public static final Resources INSTANCE = GWT.create(Resources.class);
+
+	public static final String ROOT_DIR = "com/julienvey/blog/gwtstuff/images/public/";
+
+	@Source(ROOT_DIR + "zenika1.png")
+	ImageResource zenika1Large();
+
+	@Source(ROOT_DIR + "zenika1-small.png")
+	ImageResource zenika1Small();
+
+	@Source(ROOT_DIR + "zenika1.png")
+	@ImageOptions(height = 80, width = 128)
+	ImageResource zenikaSmall();
+
+	@Source(ROOT_DIR + "zenika1.png")
+	@ImageOptions(height = 240, width = 384)
+	ImageResource zenikaMedium();
+
+	@Source(ROOT_DIR + "zenika1.png")
+	@ImageOptions(height = 640, width = 1024)
+	ImageResource zenikaLarge();
+
+}
