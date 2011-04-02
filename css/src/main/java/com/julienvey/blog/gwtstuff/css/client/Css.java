1f36548b7dd32c1986aa78590adf32dfb3c5db23
Julien Vey <vey.julien@gmail.com>
2011-04-02 18:45:03 +0200

    css Stuff begin

---
+++ css/src/main/java/com/julienvey/blog/gwtstuff/css/client/Css.java
@@ -0,0 +1,10 @@
+package com.julienvey.blog.gwtstuff.css.client;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface Css extends CssResource {
+
+	public String demo1();
+
+	public String demo2();
+}
--- css/src/main/java/com/julienvey/blog/gwtstuff/css/client/CssStuff.java
+++ css/src/main/java/com/julienvey/blog/gwtstuff/css/client/CssStuff.java
@@ -1,8 +1,7 @@
 package com.julienvey.blog.gwtstuff.css.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -10,34 +9,21 @@ public class CssStuff implements EntryPoint {
 
 	public void onModuleLoad() {
 		RootPanel root = RootPanel.get();
-		VerticalPanel main = new VerticalPanel();
-		root.add(main);
-		main.setSpacing(2);
+		VerticalPanel panel = new VerticalPanel();
+		root.add(panel);
+
+		// Inject css into the dom
+		Resources.INSTANCE.css().ensureInjected();
 
 		/* Demo 1 */
-		HorizontalPanel demo1 = new HorizontalPanel();
-		main.add(demo1);
-		demo1.setSpacing(2);
-
-		Image largeImage = new Image(CssResources.INSTANCE.zenika1Large());
-		demo1.add(largeImage);
-
-		Image smallImage = new Image(CssResources.INSTANCE.zenika1Small());
-		demo1.add(smallImage);
+		Label demo1Label = new Label("Demo 1");
+		demo1Label.addStyleName(Resources.INSTANCE.css().demo1());
+		panel.add(demo1Label);
 
 		/* Demo 2 */
-		HorizontalPanel demo2 = new HorizontalPanel();
-		main.add(demo2);
-		demo2.setSpacing(2);
-
-		Image large = new Image(CssResources.INSTANCE.zenikaLarge());
-		demo2.add(large);
-
-		Image medium = new Image(CssResources.INSTANCE.zenikaMedium());
-		demo2.add(medium);
-
-		Image small = new Image(CssResources.INSTANCE.zenika1Small());
-		demo2.add(small);
+		Label demo2Label = new Label("Demo 2");
+		demo2Label.addStyleName(Resources.INSTANCE.css().demo2());
+		panel.add(demo2Label);
 
 	}
 }
