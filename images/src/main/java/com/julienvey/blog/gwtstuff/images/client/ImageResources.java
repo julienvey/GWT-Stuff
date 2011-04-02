5e00597e382f043a5b2892cc920b3256c9eb83f7
Julien Vey <vey.julien@gmail.com>
2011-04-02 18:45:37 +0200

    refactor images

--- images/src/main/java/com/julienvey/blog/gwtstuff/images/client/ImageResources.java
+++ images/src/main/java/com/julienvey/blog/gwtstuff/images/client/ImageResources.java
@@ -9,23 +9,21 @@ public interface ImageResources extends ClientBundle {
 
 	public static final ImageResources INSTANCE = GWT.create(ImageResources.class);
 
-	public static final String ROOT_DIR = "com/julienvey/blog/gwtstuff/images/public/";
-
-	@Source(ROOT_DIR + "zenika1.png")
+	@Source("zenika1.png")
 	ImageResource zenika1Large();
 
-	@Source(ROOT_DIR + "zenika1-small.png")
+	@Source("zenika1-small.png")
 	ImageResource zenika1Small();
 
-	@Source(ROOT_DIR + "zenika1.png")
+	@Source("zenika1.png")
 	@ImageOptions(height = 80, width = 128)
 	ImageResource zenikaSmall();
 
-	@Source(ROOT_DIR + "zenika1.png")
+	@Source("zenika1.png")
 	@ImageOptions(height = 240, width = 384)
 	ImageResource zenikaMedium();
 
-	@Source(ROOT_DIR + "zenika1.png")
+	@Source("zenika1.png")
 	@ImageOptions(height = 640, width = 1024)
 	ImageResource zenikaLarge();
 
